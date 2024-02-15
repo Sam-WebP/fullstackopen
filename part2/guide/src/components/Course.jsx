5d9e586544326e7023a35e32a9f831f666dac9e2
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-02-15 20:58:58 +1100

    Have 2.3, 2.4 and 2.5 planneed out

--- part2/guide/src/components/Course.jsx
+++ part2/guide/src/components/Course.jsx
@@ -1,39 +1,57 @@
 import React from 'react'
 
-// const course = {
-//     id: 1,
-//     name: 'Half Stack application development',
-//     parts: [
-//       {
-//         name: 'Fundamentals of React',
-//         exercises: 10,
-//         id: 1
-//       },
-//       {
-//         name: 'Using props to pass data',
-//         exercises: 7,
-//         id: 2
-//       },
-//       {
-//         name: 'State of a component',
-//         exercises: 14,
-//         id: 3
-//       }
-//     ]
-//   }
+const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-
-// const Part = ({ parts }) => {
-//   return (
-//     <div>{course.parts.map(part => part.name)}</div>
-//   )
-// }
+// The way to solve this is to break up the header, part names & exercises and the total into separate modules and then put them all together into the course
 
 const Course = ({ course }) => {
   
   return (
     <>
-        <h1>{course.name}</h1>
+        <h2>{course.name}</h2>
 
         {course.parts.map(parts => 
           <div key={parts.id}>
