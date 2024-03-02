fda3c3c3123554f4d026013a8b23f274b73ffc45
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-03-02 21:18:51 +1100

    Finished 4.6 and 4.7

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -21,7 +21,65 @@ const favouriteBlog = (blogs) => {
   }
 }
 
+const mostBlogs = (blogs) => {
+  let tracker = []
+  let maxBlogs = 0
+  let maxAuthor = ''
+
+  blogs.forEach(blog => {
+    const authorIndex = tracker.findIndex(a => a.name === blog.author)
+    if (authorIndex === -1) {
+      tracker.push({ name: blog.author, blogs: 1 })
+      if (1 > maxBlogs) {
+        maxBlogs = 1
+        maxAuthor = blog.author
+      }
+    } else {
+      tracker[authorIndex].blogs += 1
+      if (tracker[authorIndex].blogs > maxBlogs) {
+        maxBlogs = tracker[authorIndex].blogs
+        maxAuthor = blog.author
+      }
+    }
+  })
+
+  return {
+    author: maxAuthor,
+    blogs: maxBlogs
+  }
+}
+
+const mostLikes = (blogs) => {
+  let tracker = []
+  let highestLikes = 0
+  let highestAuthor = ''
+
+  blogs.forEach(blog => {
+    const authorIndex = tracker.findIndex(a => a.name === blog.author)
+    if (authorIndex === -1) {
+      tracker.push({ name: blog.author, likes: blog.likes })
+      if (highestLikes === 0) {
+        highestLikes = blog.likes
+        highestAuthor = blog.author
+      }
+    } else {
+      tracker[authorIndex].likes += blog.likes
+      if (tracker[authorIndex].likes > highestLikes) {
+        highestLikes = tracker[authorIndex].likes
+        highestAuthor = blog.author
+      }
+    }
+  })
+
+  return {
+    author: highestAuthor,
+    likes: highestLikes
+  }
+}
+
 module.exports = {
   dummy,
   favouriteBlog,
+  mostBlogs,
+  mostLikes
 }
