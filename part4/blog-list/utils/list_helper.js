2223fd58d20d04d008597f5c779348d86c823540
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-03-02 18:25:56 +1100

    Finished 4.5

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,26 @@ const dummy = (blogs) => {
   return blogs.length ** 0
 }
 
+const favouriteBlog = (blogs) => {
+  let highestLikes = 0
+  blogs.map(blog => {
+    if (blog.likes >= highestLikes) {
+      highestLikes = blog.likes
+    }
+  })
+
+  const highestBlogIndex = blogs.findIndex(blog => 
+    blog.likes === highestLikes
+  )
+
+  return {
+    title: blogs[highestBlogIndex].title,
+    author: blogs[highestBlogIndex].author,
+    likes: blogs[highestBlogIndex].likes
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  favouriteBlog,
 }
