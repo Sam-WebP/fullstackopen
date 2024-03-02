f14aee13d1eeadd04bf70bfe80f56fbb88404e98
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-03-02 15:21:28 +1100

    Finished 4.2

---
+++ part4/blog-list/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
